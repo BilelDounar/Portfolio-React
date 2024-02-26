ecb00ce1efec32736ce04ec2ff2a6fed6bdeacdf
bilel.d <bileldounar@mail.com>
2024-02-26 22:21:21 +0100

    init

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import './assets/css/App.scss'
+import Header from './assets/view/header.jsx'
+
+
+
+
+function App() {
+
+
+
+  return (
+    <>
+      <Header />
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ portfolio/src/assets/view/header.jsx
@@ -0,0 +1,16 @@
+import Spline from "@splinetool/react-spline";
+
+function Header() {
+     return (
+          <>
+               <header>
+                    <Spline
+                         className="spline"
+                         scene="https://prod.spline.design/KhNitt6TBMaXZdIj/scene.splinecode"
+                    />
+               </header>
+          </>
+     );
+}
+
+export default Header;
